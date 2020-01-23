c99d70d5a934cb01141cf204e33a77bc9baed26e
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-01-23 01:14:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-goruck = {americanjacket => 325, ruckinggrid => 135, bullet => 155}
+goruck = {"americanjacket" => 325, "ruckinggrid" => 135, "bullet" => 155}
 
 def key_for_min_value(goruck)
   lowest_item = 0
